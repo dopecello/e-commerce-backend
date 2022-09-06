29ee2812226c5a7f0939d659a27429f737f2f02a
dopecello <williamlocke.cello@gmail.com>
2022-09-06 00:06:01 -0400

    updated Product model

--- models/Product.js
+++ models/Product.js
@@ -1,6 +1,6 @@
-const { DECIMAL } = require('sequelize');
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { DECIMAL } = require("sequelize");
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 class Product extends Model {}
 
@@ -10,32 +10,41 @@ Product.init(
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true
+      autoIncrement: true,
     },
     product_name: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
     price: {
       type: DataTypes.DECIMAL,
       allowNull: false,
       validate: {
-        isDecimal: true
-      }
+        isDecimal: true,
+      },
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-    }
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: "product",
   }
 );
 
