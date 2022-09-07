c8aee08fcbd379f870a498dc477e0e5fdf1b1267
dopecello <williamlocke.cello@gmail.com>
2022-09-06 23:07:58 -0400

    cleaned up file

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,10 +1,8 @@
 const router = require("express").Router();
 const { Category, Product } = require("../../models");
 
-// The `/api/categories` endpoint
 
 router.get("/", (req, res) => {
-  // find all categories
   Category.findAll({
     include: [
       {
