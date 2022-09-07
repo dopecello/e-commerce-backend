7257d4daee80fd70a975c4a652440057177fd524
dopecello <williamlocke.cello@gmail.com>
2022-09-06 23:06:11 -0400

    category delete route success

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -82,7 +82,21 @@ router.put("/:id", (req, res) => {
 });
 
 router.delete("/:id", (req, res) => {
-  // delete a category by its `id` value
-});
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbCategoryData => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: 'The category you are trying to remove does not exist!' });
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });});
 
 module.exports = router;
